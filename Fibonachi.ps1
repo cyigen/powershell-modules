7c82f65cc0b21bec6aa2d42819820508fbbf29c2
cuneyt yigen <cyigen@fintek.local>
2020-02-20 11:42:34 +0300

    initial commmit

---
+++ Fibonachi.ps1
@@ -0,0 +1,12 @@
+﻿function Fib() {
+    [CmdletBinding()]
+    param ($n)
+    $a = 0
+    $b = 1
+    for ($i = 1; $i -le $n; $i ++) {
+        $c = $a + $b
+        $a = $b
+        $b = $c
+    }
+    $c
+}
